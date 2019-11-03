52362a7d95dff355b61638383ba9c81ff5e1ea8a
michalkoziara <mkoziara12@gmail.com>
2019-11-03 12:25:13 +0100

    Migrate to AndroidX

--- app/src/main/java/c/michalkoziara/iot_mobile/RegisterActivity.java
+++ app/src/main/java/c/michalkoziara/iot_mobile/RegisterActivity.java
@@ -1,6 +1,6 @@
 package c.michalkoziara.iot_mobile;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RegisterActivity extends AppCompatActivity {

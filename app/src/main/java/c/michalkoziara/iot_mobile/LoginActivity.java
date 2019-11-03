e6145e82169d09bd40416d6a687c8cd8fa9fb60a
michalkoziara <mkoziara12@gmail.com>
2019-11-03 17:17:05 +0100

    Add UI to login and registration

--- app/src/main/java/c/michalkoziara/iot_mobile/LoginActivity.java
+++ app/src/main/java/c/michalkoziara/iot_mobile/LoginActivity.java
@@ -1,13 +1,152 @@
 package c.michalkoziara.iot_mobile;
 
+import android.app.ProgressDialog;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+
+
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    TextInputEditText _emailText;
+    TextInputLayout _emailTextLayout;
+    TextInputEditText _passwordText;
+    TextInputLayout _passwordTextLayout;
+    MaterialButton _loginButton;
+    TextView _signupLink;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        _emailText = findViewById(R.id.input_email);
+        _emailTextLayout = findViewById(R.id.input_layout_email);
+        _passwordText = findViewById(R.id.input_password);
+        _passwordTextLayout = findViewById(R.id.input_layout_password);
+        _loginButton = findViewById(R.id.btn_login);
+        _signupLink = findViewById(R.id.link_sign_up);
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        _signupLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        _loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage(getString(R.string.authorization_dialog));
+        progressDialog.show();
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: Implement successful signup logic here
+                // By default we just finish the Activity and log them in automatically
+//                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        _loginButton.setEnabled(true);
+//        finish();
+    }
+
+    public void onLoginFailed() {
+        View contextView = findViewById(R.id.login_scrollview);
+        Snackbar.make(contextView, R.string.login_failed_message, Snackbar.LENGTH_SHORT).show();
+
+        _loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailTextLayout.setError(getString(R.string.invalid_email_message));
+            _emailTextLayout.setErrorEnabled(true);
+            valid = false;
+        } else {
+            _emailTextLayout.setError(null);
+            _emailTextLayout.setErrorEnabled(false);
+        }
+
+        if (password.isEmpty()) {
+            _passwordTextLayout.setError(getString(R.string.invalid_password_message));
+            _passwordTextLayout.setErrorEnabled(true);
+            valid = false;
+        } else {
+            _passwordTextLayout.setError(null);
+            _passwordTextLayout.setErrorEnabled(false);
+        }
+
+        return valid;
     }
 }
--- app/src/main/java/c/michalkoziara/iot_mobile/RegisterActivity.java
+++ app/src/main/java/c/michalkoziara/iot_mobile/RegisterActivity.java
@@ -1,13 +1,142 @@
 package c.michalkoziara.iot_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = "RegisterActivity";
+
+    TextInputEditText _nameText;
+    TextInputLayout _nameTextLayout;
+    TextInputEditText _emailText;
+    TextInputLayout _emailTextLayout;
+    TextInputEditText _passwordText;
+    TextInputLayout _passwordTextLayout;
+    MaterialButton _signupButton;
+    TextView _loginLink;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        _nameText = findViewById(R.id.input_name);
+        _nameTextLayout = findViewById(R.id.input_layout_username);
+        _emailText = findViewById(R.id.input_email);
+        _emailTextLayout = findViewById(R.id.input_layout_email);
+        _passwordText = findViewById(R.id.input_password);
+        _passwordTextLayout = findViewById(R.id.input_layout_password);
+        _signupButton = findViewById(R.id.btn_signup);
+        _loginLink = findViewById(R.id.link_login);
+
+        _signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                finish();
+            }
+        });
+    }
+
+    public void signup() {
+        Log.d(TAG, "Signup");
+
+        if (!validate()) {
+            onSignupFailed();
+            return;
+        }
+
+        _signupButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(RegisterActivity.this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage(getString(R.string.registration_dialog));
+        progressDialog.show();
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        // TODO: Implement your own signup logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onSignupSuccess or onSignupFailed
+                        // depending on success
+                        onSignupSuccess();
+                        // onSignupFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    public void onSignupSuccess() {
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+//        finish();
+    }
+
+    public void onSignupFailed() {
+        View contextView = findViewById(R.id.register_scrollview);
+        Snackbar.make(contextView, R.string.register_failed_message, Snackbar.LENGTH_SHORT).show();
+
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+
+        if (name.isEmpty() || name.length() < 6) {
+            _nameTextLayout.setError(getString(R.string.invalid_username_message));
+            _nameTextLayout.setErrorEnabled(true);
+            valid = false;
+        } else {
+            _nameText.setError(null);
+            _nameTextLayout.setErrorEnabled(false);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailTextLayout.setError(getString(R.string.invalid_email_message));
+            _emailTextLayout.setErrorEnabled(true);
+            valid = false;
+        } else {
+            _emailTextLayout.setError(null);
+            _emailTextLayout.setErrorEnabled(false);
+        }
+
+        if (password.isEmpty() || password.length() < 8) {
+            _passwordTextLayout.setError(getString(R.string.invalid_new_password_message));
+            _passwordTextLayout.setErrorEnabled(true);
+            valid = false;
+        } else {
+            _passwordTextLayout.setError(null);
+            _passwordTextLayout.setErrorEnabled(false);
+        }
+
+        return valid;
     }
 }
